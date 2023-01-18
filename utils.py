f2fc2996feb1f517a6a284a45cfa171a5830f9a1
JoohyungLee0106 <43436960+JoohyungLee0106@users.noreply.github.com>
2023-01-18 20:16:09 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,20 +4,20 @@ from PIL import Image
 import numpy as np
 
 # 수정 포인트1
-PATH_ROOT = '/home/destin/storage/imagenet_eval/train'
+PATH_ROOT = '/mnt/aitrics_ext/ext01/shared/camelyon_large_x20/train'
 # 수정 포인트2
-path_list = glob.glob(os.path.join(PATH_ROOT, 'n15075141', '*.JPEG'))
+path_list = glob.glob(os.path.join(PATH_ROOT, '*', '*', '*.jpeg'))
 
 
 ### mean
 mean_v = np.array([0., 0., 0.])
 for path in path_list:
     img = np.array(Image.open(path))
-    # assert (img.shape[2] == 3) and (len(img.shape) == 3)
     try:
-        mean_v += np.mean(img, axis=(0, 1))
+        assert (img.shape[2] == 3) and (len(img.shape) == 3)
     except:
-        pass
+        raise ValueError(f'Invalid image property: {path}')
+    mean_v += np.mean(img, axis=(0, 1))
 
 mean_v /= len(path_list)
 
@@ -26,10 +26,7 @@ mean_v_expanded = np.expand_dims(np.expand_dims(mean_v, 0), 0)
 stddev = np.array([0., 0., 0.])
 ## all image must be in same shape (width, height)
 for path in path_list:
-    try:
-        stddev += np.mean(np.square(np.array(Image.open(path)) - mean_v_expanded), axis=(0, 1))
-    except:
-        pass
+    stddev += np.mean(np.square(np.array(Image.open(path)) - mean_v_expanded), axis=(0, 1))
 
 stddev /= len(path_list)
 stddev = np.sqrt(stddev)
