a1daad8df36ecbba6bfefca1858d3731e07f8f5f
JoohyungLee0106 <43436960+JoohyungLee0106@users.noreply.github.com>
2023-01-19 16:13:42 +0900

    centercrop

--- mean_stddev.py
+++ mean_stddev.py
@@ -4,20 +4,19 @@ from PIL import Image
 import numpy as np
 
 # 수정 포인트1
-PATH_ROOT = '/mnt/aitrics_ext/ext01/shared/camelyon_large_x20/train'
+PATH_ROOT = '/mnt/aitrics_ext/ext01/shared/hazel/cam_jpeg_train/train'
 # 수정 포인트2
 path_list = glob.glob(os.path.join(PATH_ROOT, '*', '*', '*.jpeg'))
-
 
 ### mean
 mean_v = np.array([0., 0., 0.])
 for path in path_list:
     img = np.array(Image.open(path))
     try:
-        assert (img.shape[2] == 3) and (len(img.shape) == 3)
+        assert (img.shape[0] == 384) and (img.shape[1] == 384) and (img.shape[2] == 3) and (len(img.shape) == 3)
     except:
-        raise ValueError(f'Invalid image property: {path}')
-    mean_v += np.mean(img, axis=(0, 1))
+        raise ValueError(f'Invalid image property: {path}\nshape: {img.shape}')
+    mean_v += np.mean(img[64:-64, 64:-64, :], axis=(0, 1))
 
 mean_v /= len(path_list)
 
@@ -26,7 +25,7 @@ mean_v_expanded = np.expand_dims(np.expand_dims(mean_v, 0), 0)
 stddev = np.array([0., 0., 0.])
 ## all image must be in same shape (width, height)
 for path in path_list:
-    stddev += np.mean(np.square(np.array(Image.open(path)) - mean_v_expanded), axis=(0, 1))
+    stddev += np.mean(np.square(np.array(Image.open(path))[64:-64, 64:-64, :] - mean_v_expanded), axis=(0, 1))
 
 stddev /= len(path_list)
 stddev = np.sqrt(stddev)
