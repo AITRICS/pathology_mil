d354e7cc5c7df9b0fa13afcee67d0fab8a3e1134
JoohyungLee0106 <liberaboit@hotmail.com>
2023-05-09 16:56:49 +0000

    0510

--- preprocessing.py
+++ preprocessing.py
@@ -4,164 +4,199 @@ import multiprocessing as mp
 from itertools import repeat
 import argparse
 import glob
-import openslide
-import cucim 
+from openslide import OpenSlide
 import cv2
-import math
 import numpy as np
-import pandas as pd
+import pickle
+from PIL import Image
 
-def get_mask(args,item):
-    wsi = openslide.OpenSlide(item)
-    cu_wsi = cucim.clara.CuImage(item)
-        
-    if args.mask_level == None :
-        mask_level = wsi.level_count -1
-    else :
-        mask_level = args.mask_level
-    maskimg = np.array(cu_wsi.read_region(location = (0,0),level = mask_level, num_workers= args.num_workers )) 
+
+# def generate_patch(path_wsi, pkl_dict, args):
+def generate_patch(path_wsi, pkl_dict, args):
+    """
+    0) Get mpp_np
+    1) Get level_mask, size_patch_level0
+    2) Get threshold_cb, threshold_cr using level_mask
+    3) Do for loop using size_patch_level0
+    4) (Under for loop) Get if_background
+    5) (Under for loop & Under if if_background) Save patch
+    """
+    # path_subjectID = os.path.join(args.config[args.dataset]['root_patch'], args.foldername, pkl_dict[os.path.basename(path_wsi).split('.')[0]])
+    path_subjectID = os.path.join(args.config[args.dataset]['root_patch'], pkl_dict[os.path.basename(path_wsi).split('.')[0]])
+    if not os.path.exists(path_subjectID):
+        os.makedirs(path_subjectID)
+
+    wsi = OpenSlide(path_wsi)
+    ## 0) Get mpp_np
+    # mppx and mppy are always almost-same
+    if path_wsi[-3:] == 'tif':
+        try:
+            mppx = float(10**4) / float(wsi.properties['tiff.XResolution'])
+            mppy = float(10**4) / float(wsi.properties['tiff.YResolution'])
+        except:
+            mppx = float(wsi.properties['openslide.mpp-x']) 
+            mppy = float(wsi.properties['openslide.mpp-y'])                 
+    else:
+        try:
+            mppx = float(wsi.properties['openslide.mpp-x'])
+            mppy = float(wsi.properties['openslide.mpp-y'])
+        except:                
+            raise ValueError(f'something wrong !!!!!!!!!!:\n{path_wsi}\nhas no mpp info')
     
-    # otsu thresholding in ycrcb domain
-    cvimg = cv2.cvtColor(maskimg[:,:,:3],cv2.COLOR_RGB2YCrCb)
-    _, th1 = cv2.threshold(cvimg[:, :, 1], 16, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    _, th2 = cv2.threshold(cvimg[:, :, 2], 16, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    mask = th1 | th2                     
-    mask = cv2.resize(mask, dsize=wsi.level_dimensions[args.patch_level], interpolation=cv2.INTER_CUBIC)       
-              
-    return wsi, mask, mask_level, cu_wsi
+    mppx_np = mppx * np.array(list(wsi.level_downsamples))
+    mppy_np = mppy * np.array(list(wsi.level_downsamples))
 
-def mapping(args, wsi, mask, mask_level, cu_wsi):
-    try:
-        mpp_ = wsi.properties['openslide.mpp-x']
-    except :
-        print(f'{args.imgName} : no mpp info')
-        return
+    ## 1) Get level_mask, size_patch_level0
+    level_mask = np.argmin(np.absolute(mppx_np-args.config[args.dataset]['mpp_mask']))
+    assert level_mask == (np.argmin(np.absolute(mppy_np-args.config[args.dataset]['mpp_mask'])))   
+
+    size_patch_level0_x = round(args.config[args.dataset]['size_patch'] * args.config[args.dataset]['mpp_patch'] / mppx)
+    size_patch_level0_y = round(args.config[args.dataset]['size_patch'] * args.config[args.dataset]['mpp_patch'] / mppy)
+
+    ## 2) Get threshold_cb (th_cb, 크면 foreground), threshold_cr (th_cr, 크면 foreground) using level_mask
+
+    mask_pl = wsi.read_region(location=(0,0), level=level_mask,
+                        size=(int(wsi.properties[f'openslide.level[{level_mask}].width']), int(wsi.properties[f'openslide.level[{level_mask}].height']))).convert('YCbCr')
+    mask_np = np.array(mask_pl)
+    th_cb, _ = cv2.threshold(mask_np[:, :, 1], 80, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    th_cr, _ = cv2.threshold(mask_np[:, :, 2], 80, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    ## 3) Get list_idx_width, list_idx_height, threshold_background. 0 dimension of numpy is height
+
+    w_level0, h_level0 = wsi.level_dimensions[0]
+    list_idx_width = list(range(0, w_level0, size_patch_level0_x))
+    list_idx_height = list(range(0, h_level0, size_patch_level0_y))
+
+    if w_level0 % size_patch_level0_x != 0:
+        list_idx_width.pop(-1)
+        list_idx_width.append(w_level0-size_patch_level0_x)
+    if h_level0 % size_patch_level0_y != 0:
+        list_idx_height.pop(-1)
+        list_idx_height.append(h_level0-size_patch_level0_y)
+
+    threshold_background = size_patch_level0_x * size_patch_level0_y * args.threshold_mask
+    kernel = np.ones((32, 32), np.uint8)
+
+    ## 4) Do for loop using size_patch_level_patch
+    for i in list_idx_width:
+        for j in list_idx_height:
+            patch_pl = wsi.read_region(location=(i,j), level=0, size=(size_patch_level0_x, size_patch_level0_y))
+            patch_ycbcr_np = np.array(patch_pl.convert('YCbCr'))
+    ## 5) Get if_background
+            
+            mask_cb = cv2.threshold(patch_ycbcr_np[:,:,1], thresh=th_cb, maxval=1, type=cv2.THRESH_BINARY)
+            mask_cr = cv2.threshold(patch_ycbcr_np[:,:,2], thresh=th_cr, maxval=1, type=cv2.THRESH_BINARY)
+            mask = mask_cb[1] | mask_cr[1]
+            
+            mask = cv2.erode(cv2.dilate(mask, kernel, iterations=1), kernel, iterations=1)
+            mask = cv2.dilate(cv2.erode(mask, kernel, iterations=1), kernel, iterations=1)
+            # print(f'min({np.min(mask)}), max({np.max(mask)})')
+    # 4) (Under for loop) Get if_background
+            if mask.sum() >= threshold_background:
+
+    # 5) (Under for loop & Under if if_background) Save patch    
+                # patch_pl.resize((args.config[args.dataset]['size_patch'], args.config[args.dataset]['size_patch']), Image.Resampling.LANCZOS).save(os.path.join(args.config[args.dataset]['root_patch'], args.foldername, pkl_dict[os.path.basename(path_wsi).split('.')[0]], f'w{i}_h{j}.jpeg'))
+                patch_pl = patch_pl.resize((args.config[args.dataset]['size_patch'], args.config[args.dataset]['size_patch']), Image.Resampling.LANCZOS).convert('RGB')
+                patch_pl.save(os.path.join(args.config[args.dataset]['root_patch'], pkl_dict[os.path.basename(path_wsi).split('.')[0]], f'w{i}_h{j}.jpeg'))
+
+def main(args):
+
+    path_folder = args.config[args.dataset]['root_patch']
+    if not os.path.exists(path_folder):
+        os.mkdir(path_folder)
+
+    with open(f'preprocess/{args.config[args.dataset]["pkl"]}_train.pkl', 'rb') as fp:
+        pkl_tr = pickle.load(fp)
+    with open(f'preprocess/{args.config[args.dataset]["pkl"]}_test.pkl', 'rb') as fp:
+        pkl_test = pickle.load(fp)
+
+    pkl = {}
+    pkl.update(pkl_tr)
+    pkl.update(pkl_test)
+
+    pathlist_wsi = glob.glob(args.config[args.dataset]['path_list_wsi'])
+
+    # check if any subject is missing
+    for _subjectID in pkl.keys():
+        if_wsi_exist = False
+        for path_wsi in pathlist_wsi:
+            if os.path.basename(path_wsi).split('.')[0] == _subjectID:
+                if_wsi_exist = True
+        if if_wsi_exist == False:
+            raise ValueError(f'WSI file does NOT exist for the followin subject ID: {_subjectID}')
     
-    if args.data == '16':
-        ref = pd.read_csv('reference.csv', names=['name', 'class', 'subclass', 'binary'])
-        if 'test' in args.imgName :
-            _class = ref['class'][ref['name'].str.contains(args.imgName)]
-    if args.data == 'tcga':
-        if not os.path.isdir(os.path.join(args.dest, args.imgName)):
-            os.makedirs(os.path.join(args.dest, args.imgName), exist_ok=True)
+    for path_wsi in pathlist_wsi:
+        if_pkl_exist = False
+        _subjectID = os.path.basename(path_wsi).split('.')[0]
+        for _subjectID_pkl in pkl.keys():
+            if _subjectID == _subjectID_pkl:
+                if_pkl_exist = True
+        if if_pkl_exist == False:
+            pathlist_wsi.remove(path_wsi)
 
-    hp_0 = args.patch_size * args.mpp / float(mpp_) 
-    hp = int(hp_0 / wsi.level_downsamples[args.patch_level]) # patch size for high resolution
-    mp = int(hp_0 / wsi.level_downsamples[mask_level]) # patch size for low resolution otsu mask
-    if args.large_patch_size != None:
-        hp_0_large = args.large_patch_size * args.mpp / float(mpp_)
-        hp_large = int(hp_0_large / wsi.level_downsamples[args.patch_level])
+    p = mp.Pool(os.cpu_count())
+    p.starmap(generate_patch, zip(pathlist_wsi, repeat(pkl), repeat(args)))
+    # for path_wsi in pathlist_wsi:
+    #     generate_patch(path_wsi, pkl, args)
 
-    count = 0
-    w,h = wsi.level_dimensions[mask_level] 
-    for i in range(math.floor(w/mp)):
-        for j in range(math.floor(h/mp)):
-            if mask[j*hp:(j+1)*hp, i*hp:(i+1)*hp].sum() >= hp*hp* args.mask_threshold*255 :
-                if args.large_patch_size is None:
-                    patch = np.array(cu_wsi.read_region(location = (hp_0*i, hp_0*j),level = args.patch_level, size = (hp,hp), num_workers = 5))[:,:,:3]
-                    resized = cv2.resize(patch, dsize=(args.patch_size, args.patch_size))
-                else :
-                    patchlev_lefttop = (i*hp + (hp-hp_large)/2, j*hp + (hp-hp_large)/2)
-                    lev0_lefttop = (patchlev_lefttop[0]* wsi.level_downsamples[args.patch_level], patchlev_lefttop[1]* wsi.level_downsamples[args.patch_level])
-                    patch = np.array(cu_wsi.read_region(location = lev0_lefttop ,level = args.patch_level,size = (hp_large,hp_large), num_workers = 5))[:,:,:3]
-                    resized = cv2.resize(patch, dsize=(args.large_patch_size, args.large_patch_size))                
-                count +=1
-                
-                if args.data == 'tcga':
-                    cv2.imwrite(os.path.join(args.dest, args.imgName,'patch_{}.jpeg'.format(count)),resized)
-                else : # camelyon16
-                    if 'test' in args.imgName :
-                        _class = ref['class'][ref['name'].str.contains(args.imgName)]
-                        if not os.path.isdir(os.path.join(args.dest,"test",_class.item(), args.imgName)):
-                            os.makedirs(os.path.join(args.dest,"test",_class.item(), args.imgName))
-                        cv2.imwrite(os.path.join(args.dest,"test",_class.item(), args.imgName,'patch_{}.jpeg'.format(count)),resized)
-
-                    elif 'normal' in args.imgName : 
-                        if not os.path.isdir(os.path.join(args.dest,"train/normal", args.imgName)) :
-                            os.makedirs(os.path.join(args.dest,"train/normal", args.imgName))
-                        cv2.imwrite(os.path.join(args.dest,"train/normal", args.imgName,'patch_{}.jpeg'.format(count)),resized)
-                    elif 'tumor' in args.imgName : 
-                        if not os.path.isdir(os.path.join(args.dest,"train/tumor", args.imgName)) :
-                            os.makedirs(os.path.join(args.dest,"train/tumor", args.imgName))
-                        cv2.imwrite(os.path.join(args.dest,"train/tumor", args.imgName,'patch_{}.jpeg'.format(count)),resized)         
-                                            
-    print(f'Patch collection is done for {args.imgName}, {count} patches are collected')
-        
-def iter_data(item, args):
-    if args.data == 'tcga':
-        args.imgName = item.split('/')[-2] + '/' + item.split('/')[-1].split('.')[0] # including labels
-    else : # camelyon16
-        args.imgName = item.split('/')[-1].split('.')[0]
-    
-    print("*************{}**************".format(args.imgName))
-    wsi, mask, mask_level, cu_wsi = get_mask(args, item)
-    mapping(args,wsi, mask, mask_level, cu_wsi)
-        
-
-
-def patch_generation(args): 
-    if args.data == 'tcga':
-        data_list = glob.glob(args.wsi_root+'/*/*') 
-    else : # camelyon16
-        data_list = glob.glob(args.wsi_root+'/*')
-    print(f'{len(data_list)} slides found')
-    
-    if args.multiprocess :
-        p = mp.Pool(args.num_process)
-        p.starmap(iter_data,zip(data_list,repeat(args)))
-    else : 
-        for item in data_list :
-            iter_data(item, args)
 
 def parse_args():
 
-    parser = argparse.ArgumentParser(description="Multiple Instance Learning (MIL) example of classification from WSI.")
- 
-    parser.add_argument("--imgName", default=None , type= str)
-    parser.add_argument("--wsi_root", default = None, help = 'path to root folder of CAMELYON WSI image')
-    parser.add_argument("--data", choices=['CAMELYON16','tcga'], type = str)
-    parser.add_argument("--dest", default="/mnt/aitrics_ext/ext01/shared/tcga_stad_wsi", help="path to root folder of camelyon images")
-
-    parser.add_argument("--patch_size", default = 256, type =int, help="The number of pixel that you want for the patches")
-    parser.add_argument("--large_patch_size", default= None, type = int)
-    parser.add_argument("--mask_ratio", default = None, type= float, help='ratio between level 0 and mask level')
-    parser.add_argument("--patch_ratio", default = None, type= float, help = 'ratio between level 0 and patch level')
-    parser.add_argument("--mask_level", default = None, type= int)
-    parser.add_argument("--patch_level", default = 1, type= int)
-    parser.add_argument("--mask_threshold", default = 0.75, type= float, help = 'minimum portion of foreground to keep certain patch')
-    parser.add_argument("--mpp", default= 1, type = float, help= 'desired mpp')
-
-    parser.add_argument("--multiprocess", action="store_true", help = 'Use multiprocessing while extracting patch from WSI')
-    parser.add_argument("--num_process", default=20, type=int)
-    parser.add_argument("--passifdone", action= "store_true", help = "Skip preprocessing if already done")
-    parser.add_argument("--num_workers",default=4, type=int, help='the number of workers for reading whole slide image')
+    parser = argparse.ArgumentParser(description="Generate Patches from WSI")
+    parser.add_argument("--foldername", default="camelyon16_jpeg_new", help="foldername")
+    parser.add_argument("--dataset", default='camelyon', choices=['camelyon', 'tcga_lung', 'tcga_stad'], help="dataset type")
+    parser.add_argument("--threshold-mask", default = 0.25, type= float, help = 'minimum portion of foreground to keep certain patch')
     args = parser.parse_args()
     return args
-    
-if __name__ =="__main__":
-    '''
-    wsi-root
-    camelyon16 : 하위에 바로 wsi
-    tcga : 하위에 label/wsi
-    '''
+
+# min(level count):
+# Camelyon16: 8, 28.97 mpp --> 30 mpp와 가장 가까운 mpp 레벨에서 마스크 만들기
+# tcga_lung: 2, 1.966 mpp --> 2 mpp와 가장 가까운 mpp 레벨에서 마스크 만들기
+# tcga_stad: 2, 1.975 mpp --> 2 mpp와 가장 가까운 mpp 레벨에서 마스크 만들기.
+#  그런데 그냥 위쪽 레벨로 (마스크 레벨) threshold만 구하고 마스크는 안 구하면 안되나? threshold 있으면 패치상에서도 할 수 있으니.
+# 레벨당 마스크 threshold 바뀌는지 확인 -> openslide 내에서는 별로 안바뀜 (마지막에서 두번째 레벨 쓰자)
+
+if __name__ == "__main__":
     run_start = time.time()
     args = parse_args()
+    # args.config = {'camelyon': {'mpp_mask': 30, 'mpp_patch': 0.5, 'size_patch': 256, 'ext': '.tif', 'path_list_wsi': '/nfs/thena/shared/camelyon/CAMELYON16/images/*', 'root_patch': '/nfs/thena/shared/', 'pkl': 'subjectID_camelyon16'},
+    #             'tcga_lung': {'mpp_mask': 2, 'mpp_patch': 0.5, 'size_patch': 256, 'ext': '.svs', 'path_list_wsi': '/nfs/thena/shared/tcga_lung/*/*', 'root_patch': '/nfs/thena/shared/', 'pkl': 'subjectID_tcgalung'},
+    #             'tcga_stad': {'mpp_mask': 2, 'mpp_patch': 1.0, 'size_patch': 512, 'ext': '.svs', 'path_list_wsi': '/nfs/thena/shared/tcga_stad_wsi/*/*', 'root_patch': '/nfs/thena/shared/', 'pkl': 'subjectID_tcgastadher2'}}
     
-    #debug
-    args.data = 'CAMELYON16'
-    args.patch_level = 1
-    args.mpp = 1
-    args.patch_size = 256
-    args.large_patch_size = 384
-    args.dest = '.'
-    args.wsi_root = '/nfs/thena/shared/camelyon/CAMELYON16/images'
-    # args.multiprocess = True
-    
-    
+    args.config = {'camelyon': {'mpp_mask': 30, 'mpp_patch': 0.5, 'size_patch': 224, 'ext': '.tif', 'path_list_wsi': '/mnt/aitrics_ext/ext01/shared/camelyon/CAMELYON16/images/*', 'root_patch': '/mnt/aitrics_ext/ext01/shared/', 'pkl': 'subjectID_camelyon16'},
+                'tcga_lung': {'mpp_mask': 2, 'mpp_patch': 0.5, 'size_patch': 224, 'ext': '.svs', 'path_list_wsi': '/mnt/aitrics_ext/ext01/shared/tcga_lung/*/*', 'root_patch': '/mnt/aitrics_ext/ext01/shared/', 'pkl': 'subjectID_tcgalung'},
+                'tcga_stad': {'mpp_mask': 2, 'mpp_patch': 1.0, 'size_patch': 448, 'ext': '.svs', 'path_list_wsi': '/mnt/aitrics_ext/ext01/shared/tcga_stad_wsi/*/*', 'root_patch': '/mnt/aitrics_ext/ext01/shared/', 'pkl': 'subjectID_tcgastadher2'}}
+
+
+    args.config[args.dataset]['root_patch'] += args.foldername
+
+    if os.path.isdir(args.config[args.dataset]['root_patch']):
+        raise ValueError('Invalid foldername!!! Foldername already exists!!')
+
     print("Argument values:")
     for k, v in vars(args).items():
         print(k, "=>", v)
     print("-----------------")
-    patch_generation(args)
+    main(args)
     print(f'All done in total {time.time() - run_start} sec')
 
+# https://github.com/YuZhang-SMU/Cancer-Prognosis-Analysis/blob/main/DC_MIL%20Code/preprocessing/extract_tiles.py
+# https://github.com/binli123/dsmil-wsi/blob/master/deepzoom_tiler.py
+
+# /mnt/aitrics_ext/ext01/shared/camelyon/CAMELYON16/images/{'normal_xxx.tif', 'tumor_xxx.tif', 'test_xxx.tif'}
+# /mnt/aitrics_ext/ext01/shared/tcga_lung/{'AD', 'SC'}/{'TCGA-49-4512-01Z-00-DX7.svs'}
+# /mnt/aitrics_ext/ext01/shared/tcga_stad_wsi/{'amplified', 'normal'}/{'TCGA-D7-6820-01Z-00-DX2.svs'}
+
+
+# data = {
+#     'subjectID_camelyon16_train': '/nfs/thena/shared/cam_jpeg/train/*/*',
+#     'subjectID_camelyon16_test': '/nfs/thena/shared/cam_jpeg/test/*/*',
+    
+#     'subjectID_tcgalung_train': '/nfs/thena/shared/tcga_lung_jpeg/train/fold*/*/*',
+#     'subjectID_tcgalung_test': '/nfs/thena/shared/tcga_lung_jpeg/test/*/*',
+    
+#     'subjectID_tcgastadher2_train': '/nfs/thena/shared/tcga_stad_jpeg/train/fold*/*/*',
+#     'subjectID_tcgastadher2_test': '/nfs/thena/shared/tcga_stad_jpeg/test/*/*'
+#     }
+
+# /nfs/thena/shared/camelyon/CAMELYON16/images/*
