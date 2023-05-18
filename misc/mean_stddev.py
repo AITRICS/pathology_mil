ae0d0517195bdf7f1439c620b05a3775ac2e88f7
JoohyungLee0106 <liberaboit@hotmail.com>
2023-05-18 12:21:51 +0900

    torch instead of numpy

--- misc/mean_stddev.py
+++ misc/mean_stddev.py
@@ -2,32 +2,37 @@ import glob
 import os
 from PIL import Image
 import numpy as np
+import torch
+import torchvision
 
 # 수정 포인트1
-PATH_ROOT = '/mnt/aitrics_ext/ext01/shared/hazel/cam_jpeg_train/train'
+PATH_ROOT = '/mnt/aitrics_ext/ext01/shared/camelyon16_eosin_224/train'
 # 수정 포인트2
-path_list = glob.glob(os.path.join(PATH_ROOT, '*', '*', '*.jpeg'))
-
+path_list = glob.glob(os.path.join(PATH_ROOT, '*', '*', '*', '*.jpeg'))
+# transform = T.Compose([
+#         T.ToTensor()
+#     ])
 ### mean
-mean_v = np.array([0., 0., 0.])
+totensor = torchvision.transforms.ToTensor()
+mean_v = torch.tensor([0., 0., 0.])
 for path in path_list:
-    img = np.array(Image.open(path))
+    img = totensor(Image.open(path))
     try:
-        assert (img.shape[0] == 384) and (img.shape[1] == 384) and (img.shape[2] == 3) and (len(img.shape) == 3)
+        assert (img.shape[0] == 3) and (img.shape[1] == 224) and (img.shape[2] == 224) and (len(img.shape) == 3)
     except:
         raise ValueError(f'Invalid image property: {path}\nshape: {img.shape}')
-    mean_v += np.mean(img[64:-64, 64:-64, :], axis=(0, 1))
+    mean_v += torch.mean(img, axis=(1, 2))
 
 mean_v /= len(path_list)
 
 ### stddev
-mean_v_expanded = np.expand_dims(np.expand_dims(mean_v, 0), 0)
-stddev = np.array([0., 0., 0.])
+mean_v_expanded = torch.unsqueeze(torch.unsqueeze(mean_v, 1), 2)
+stddev = torch.tensor([0., 0., 0.])
 ## all image must be in same shape (width, height)
 for path in path_list:
-    stddev += np.mean(np.square(np.array(Image.open(path))[64:-64, 64:-64, :] - mean_v_expanded), axis=(0, 1))
+    stddev += torch.mean(torch.square(totensor(Image.open(path)) - mean_v_expanded), dim=(1, 2))
 
 stddev /= len(path_list)
-stddev = np.sqrt(stddev)
+stddev = torch.square(stddev)
 
 print(f'mean: {mean_v}\nstddev: {stddev}')
