b093c7311d76b4c380e5c777050dac387c2acfb9
JoohyungLee0106 <liberaboit@hotmail.com>
2023-07-30 21:03:07 +0900

    0730 hazel

--- models/mil.py
+++ models/mil.py
@@ -60,8 +60,8 @@ class MilBase(nn.Module):
         self.weight_cov = weight_cov
         self.auxloss_weight = auxloss_weight
         #self.instance_classifier = Classifier_instance(dim_latent, num_head=128, aux_head=aux_head)
-        #self.instance_classifier = Classifier_instance(dim_in, num_head=2, aux_head=aux_head) # dsmil
-        self.instance_classifier = Classifier_instance(dim_latent, num_head=2, aux_head=aux_head) # attention, gatedattention
+        self.instance_classifier = Classifier_instance(dim_in, num_head=2, aux_head=aux_head) # dsmil
+        #self.instance_classifier = Classifier_instance(dim_latent, num_head=2, aux_head=aux_head) # attention, gatedattention
         self.scaler = torch.cuda.amp.GradScaler()
         self.sigmoid = nn.Sigmoid()
         
