06b3c7ccc20ed541c3fbdde550aa94e4d02f76b3
JoohyungLee0106 <liberaboit@hotmail.com>
2023-08-04 11:15:56 +0900

    for your convenience

--- train.sh
+++ train.sh
@@ -1,19 +1,30 @@
+## ic --> instance classifier
+## nc --> negative centroid
+
 gpu=0
 data_root=/mnt/aitrics_ext/ext01/shared/camelyon16_eosin_224_16_pkl_0524/swav_res50
+# single도 해볼 수 있음
 scheduler_centroid=None
 dataset=CAMELYON16
-train_instance=intrainstance_vc
+# None, intrainstance_divdis, interinstance_vc, interinstance_cosine, intrainstance_vc, intrainstance_cosine
+train_instance=intrainstance_vc 
+# intrainstance_divdis 하면 당연히 ic_num_head 1
 ic_num_head=5
+# intrainstance_divdis 하면 ic_num_head 1
 ic_depth=2
 weight_agree=1.0
 weight_disagree=1.0
+# 0도 해봄직
 weight_cov=1.0
 stddev_disagree=1.0
+# negative centroid 업데이트 방법: sgd, adamw, adam
 optimizer_nc=sgd
 lr=0.003
 lr_center=0.0001
+# Dtfd, Attention, GatedAttention 가능
 mil_model=Dtfd
 
+# 이렇게 하면 8개 돔. gpu 0부터 시작
 for weight_cov in 0 1; do
     for lr in 0.01 0.003 0.001 0.0003; do
 
