cb7d328eb4d9ad93a0f552b42070aa3f563c684c
JoohyungLee0106 <43436960+JoohyungLee0106@users.noreply.github.com>
2023-10-20 10:37:57 +0900

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-PYTHONHASHSEED=1 python preprocessing.py --foldername camelyon16_jpeg_new --dataset camelyon --threshold-mask 0.2
-
-PYTHONHASHSEED=1 python preprocessing.py --foldername tcgastad_jpeg_new --dataset tcga_stad --threshold-mask 0.2
-
-PYTHONHASHSEED=1 python preprocessing.py --foldername tcgalung_jpeg_new --dataset tcga_lung --threshold-mask 0.2
